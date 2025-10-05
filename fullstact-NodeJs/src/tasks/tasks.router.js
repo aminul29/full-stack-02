86eb64adde16f028cd2b2a42d52139f6cf1b94cc
aminul29 <89416308+aminul29@users.noreply.github.com>
2025-10-06 00:52:46 +0600

    refactor: modularize task validation in router
    - Replaced inline validation with createTaskValidator module
    - Removed duplicate validation logic from router file
    - Improved code organization and maintainability
    - Cleaner router code by separating concerns

--- fullstact-NodeJs/src/tasks/tasks.router.js
+++ fullstact-NodeJs/src/tasks/tasks.router.js
@@ -2,17 +2,15 @@ const express = require("express");
 const { body, validationResult } = require("express-validator"); 
 const { StatusCodes } = require("http-status-codes");
 const tasksController = require("./tasks.controller.js");
+const createTaskValidator = require("./validators/createTask.validator.js");
+
 const tasksRouter = express.Router();
 
 // Get all tasks from database
 tasksRouter.get("/tasks", tasksController.handleGetTasks);
 
 // Create a new task in database
-tasksRouter.post("/tasks", [
-    body("title", "The title cannot be empty").notEmpty(),
-    body("title", "The title must be a string").isString(),
-    body("dueDate", "Due date needs to be a valid ISO08601 string").notEmpty().isISO8601(),
-], (req, res) => {
+tasksRouter.post("/tasks", createTaskValidator, (req, res) => {
     const result = validationResult(req);
     if(result.isEmpty()){
         return tasksController.handlePostTasks(req, res);
